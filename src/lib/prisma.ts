7a779f45ce36ed37586967dc993bf6a8aeb3d866
Chiki-tella <christellamunezeroimpano@gmail.com>
2026-05-04 20:11:44 +0200

    Added websocket setup: allowing the app to tunnel to neon database in the cloud

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,25 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { Pool, neonConfig } from "@neondatabase/serverless";
+import ws from "ws";
+
+// Set the WebSocket constructor for local Node.js environment
+neonConfig.webSocketConstructor = ws;
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
+const connectionString = process.env.DATABASE_URL!;
+
+const pool = new Pool({ connectionString });
+// We cast to any here because of a slight type mismatch in the current version of the Neon adapter types
+const adapter = new PrismaNeon(pool as any);
+
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
+    adapter,
     log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
 
